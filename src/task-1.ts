07ed45c7b56c20b26f27291dd64a2fd94ed1d3aa
Katargina <katrin-kate@ukr.net>
2026-04-17 16:52:58 +0300

    tasks 1-4

---
+++ src/task-1.ts
@@ -0,0 +1,7 @@
+const name: { name: string } = { name: "Alice" }; 
+const age: { age: number } = { age: 30 }; 
+const isOnline: { isOnline: boolean } = { isOnline: true }; 
+
+console.log(`Name: ${name.name}`); 
+console.log(`Age: ${age.age}`); 
+console.log(`Online: ${isOnline.isOnline}`);
