2facbd374fc4cf4b2cf41c67e8f91196ef1dbee6
Katargina <katrin-kate@ukr.net>
2026-04-18 00:44:53 +0300

    err fixed

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,7 +1,7 @@
-const name: { name: string } = { name: "Alice" }; 
-const age: { age: number } = { age: 30 }; 
-const isOnline: { isOnline: boolean } = { isOnline: true }; 
+const name: string  = "Alice"; 
+const age: number = 30; 
+const isOnline: boolean = true; 
 
-console.log(`Name: ${name.name}`); 
-console.log(`Age: ${age.age}`); 
-console.log(`Online: ${isOnline.isOnline}`);
+console.log(`Name: ${name}`); 
+console.log(`Age: ${age}`); 
+console.log(`Online: ${isOnline}`);
